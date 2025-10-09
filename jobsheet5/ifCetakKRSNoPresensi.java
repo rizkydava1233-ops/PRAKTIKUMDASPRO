65f8a08d1bc6043a86d9e8e14eb339af5b9cd6f5
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-10-09 19:59:25 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRSNoPresensi.java
+++ jobsheet5/ifCetakKRSNoPresensi.java
@@ -11,9 +11,12 @@ public class ifCetakKRSNoPresensi {
 
 
         boolean uktLunas = sc.nextBoolean();
-       if (uktLunas) {
-            System.out.println("Pembayaran UKT terverikasi");
+      if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } 
+        else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
     
         sc.close();
