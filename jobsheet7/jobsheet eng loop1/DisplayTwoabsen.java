6dfba47d99a034a9f18174a06ff89c88d4bef511
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-10-21 10:16:21 +0700

    modify 2

--- jobsheet7/jobsheet eng loop1/DisplayTwoabsen.java
+++ jobsheet7/jobsheet eng loop1/DisplayTwoabsen.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class DisplayTwoabsen {    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-          
+       
         int numInput;
-        
+    
         System.out.print("Input some number: ");
         numInput = sc.nextInt();
         
+       
         for (int i = 2; i <= numInput; i += 2) {
-            System.out.println("2 multiple: " + i);
+            System.out.print(i + " ");
         }
-
+          
+     
         sc.close();
 
     }
