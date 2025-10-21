02b9a9c3ecb7597393f0dfc57ae7db9ed5e37acc
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-10-21 10:45:27 +0700

    Assignment (Open Challenge)

--- jobsheet7/jobsheet eng loop1/TheTriangleNoAbsen.java
+++ jobsheet7/jobsheet eng loop1/TheTriangleNoAbsen.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class TheTriangleNoAbsen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-          int numInput;
-        
-        String s = "";
+         int numInput;
         
         System.out.print("Input some number: ");
         numInput = sc.nextInt();
         
-          for (int i = 0; i < numInput; i++) {
-            s += " *";
-            System.out.println(s);
+        // Modified to create inverted triangle pattern
+        for (int i = numInput; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+       
         sc.close();
     }
     
