430b8411e4e8a65ffc2d0980d063a43873197383
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-10-21 10:29:45 +0700

    LAB 4

---
+++ jobsheet7/jobsheet eng loop1/DoWhileLeaveEntitlementNoAbsen.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DoWhileLeaveEntitlementNoAbsen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int leaveEntitlement = 12; 
+        int numLeave;
+        String confirmation;
+        
+        do {
+            System.out.print("Do you want to take a leave (y/n)? ");
+            confirmation = sc.next();
+            
+            if(confirmation.equalsIgnoreCase("y")) {
+                System.out.print("How many day(s)? ");
+                numLeave = sc.nextInt();
+                
+                if(numLeave <= leaveEntitlement) {
+                    leaveEntitlement -= numLeave;
+                    System.out.println("Remaining leave entitlement: " + leaveEntitlement);
+                } else {
+                    System.out.println("You dont have enough leave entitlement");
+                    break;
+                }
+            }
+            
+        } while(leaveEntitlement > 0);
+
+        sc.close();       
+    }
+    
+}
