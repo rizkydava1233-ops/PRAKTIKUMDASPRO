a35befb92a8e70220ffa665c79382ea2383eb88b
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-10-28 08:39:32 +0700

    percobaan 2

---
+++ jobsheet8/square.java
@@ -0,0 +1,19 @@
+package jobsheet8;
+import java.util.Scanner;
+public class square {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.print("Masukkan nilai N: ");
+        int N = sc.nextInt();
+        
+         for (int i = 0; i < N; i++) { 
+            for (int j = 0; j < N; j++) {
+                System.out.print("*");
+            } 
+            System.out.println();
+        }
+        
+        sc.close();
+    }   
+    
+}
