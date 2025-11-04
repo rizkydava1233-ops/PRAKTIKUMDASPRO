31e4caf481eb543cae970a51364b3a3b5df659c5
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-11-04 08:19:51 +0700

    modify2

--- jobsheet9/arrayNumbers07.java
+++ jobsheet9/arrayNumbers07.java
@@ -4,11 +4,11 @@ public class arrayNumbers07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] num = {5, 12, 7, 20};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
 
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]); 
-        System.out.println(num[3]);
+      
       
         sc.close();
 
