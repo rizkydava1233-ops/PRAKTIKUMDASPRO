42b6ec234ded348f31aa249d917076b25aa1d92f
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-11-04 08:31:53 +0700

    modify 1

--- arrayValue07.java
+++ arrayValue07.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 public class arrayValue07 {
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);   
+        // Create an array of integer type with the name finalScore, with a capacity of 10 elements
         int[] finalScore = new int[10];
         
-        for (int i = 0; i < 10; i++) {
+        // Using a loop, create an input to fill in the finalScore array element
+        for (int i = 0; i < finalScore.length; i++) {
             System.out.print("Enter the final score " + i + ": ");
             finalScore[i] = sc.nextInt();
         }
-             
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Final score " + i + " is " + finalScore[i]);
+        
+        // Display all students and mark which one passed, and which did not
+        System.out.println("\nFinal Results:");
+        for (int i = 0; i < finalScore.length; i++) {
+            if (finalScore[i] > 70) {
+                System.out.println("Student " + i + " with score " + finalScore[i] + " - Passed!");
+            } else {
+                System.out.println("Student " + i + " with score " + finalScore[i] + " - Did not pass");
+            }
         }
+
         
         sc.close();
     }
