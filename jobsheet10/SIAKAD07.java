523ab1d5bdf9106bd78146f3890316cf138b2dee
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-11-11 10:07:27 +0700

    modify nilai rata rata

--- jobsheet10/SIAKAD07.java
+++ jobsheet10/SIAKAD07.java
@@ -19,22 +19,22 @@ public class SIAKAD07 {
             }
         }
         
-        // Modifikasi kode program pada langkah 6 untuk menghitung dan menampilkan
-        // nilai rata-rata setiap mata kuliah
+        // nilai rata-rata setiap siswa
         System.out.println("\n====================================");
-        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+        System.out.println("Rata-rata Nilai setiap Mahasiswa:");
         
-        for (int j = 0; j < 3; j++) {
-            double totalPerMatkul = 0;
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
             
-            for (int i = 0; i < 4; i++) {
-                totalPerMatkul += nilai[i][j];
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
             }
             
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
         }
-        
-        sc.close();
     }
     
 }
