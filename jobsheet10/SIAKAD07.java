b4a55a1264ea2047b6f972b3fdd7f922ce08f569
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-11-11 10:19:46 +0700

    percobaan 4

--- jobsheet10/SIAKAD07.java
+++ jobsheet10/SIAKAD07.java
@@ -6,25 +6,20 @@ public class SIAKAD07 {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // Buat array of int bernama nilai dengan 4 baris dan 3 kolom
-        int[][] nilai = new int[4][3];
+        // Input jumlah siswa dan mata kuliah secara dinamis
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+        
+        // Buat array dengan ukuran dinamis
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
         
         // Gunakan scanner dan nested loop untuk mengisi elemen pada array nilai
+        // dan menghitung nilai rata-rata setiap siswa
         for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
-            
-            for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
-                nilai[i][j] = sc.nextInt();
-            }
-        }
-        
-        // nilai rata-rata setiap siswa
-        System.out.println("\n====================================");
-        System.out.println("Rata-rata Nilai setiap Mahasiswa:");
-        
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
             
             for (int j = 0; j < nilai[i].length; j++) {
@@ -33,8 +28,24 @@ public class SIAKAD07 {
                 totalPerSiswa += nilai[i][j];
             }
             
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/jumlahMatkul);
         }
+        
+        // Tampilkan rata-rata nilai setiap mata kuliah
+        System.out.println("\n====================================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+        
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPerMatkul = 0;
+            
+            for (int i = 0; i < jumlahSiswa; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+            
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jumlahSiswa);
+        }
+        
+        sc.close();
     }
-    
+
 }
