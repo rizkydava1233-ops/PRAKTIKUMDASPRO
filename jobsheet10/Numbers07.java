58659f5ccf4cdb9430ec17b413c2679a6c7ef373
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-11-11 09:44:02 +0700

    modify menggunakan array,toString()

---
+++ jobsheet10/Numbers07.java
@@ -0,0 +1,29 @@
+package jobsheet10;
+
+import java.util.Arrays;
+
+public class Numbers07 {
+    public static void main(String[] args) {
+    int[][] myNumbers = new int[3][];
+        
+        myNumbers[0] = new int[5];
+        
+        myNumbers[1] = new int[3];
+        
+        myNumbers[2] = new int[1];
+        
+        // Tampilkan panjang setiap baris
+        System.out.println("Jumlah baris: " + myNumbers.length);
+        System.out.println("Panjang baris ke-1: " + myNumbers[0].length + " kolom");
+        System.out.println("Panjang baris ke-2: " + myNumbers[1].length + " kolom");
+        System.out.println("Panjang baris ke-3: " + myNumbers[2].length + " kolom");
+
+        
+        // Tampilkan isi array menggunakan Arrays.toString()
+        System.out.println("\nIsi array myNumbers:");
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Baris ke-" + (i+1) + ": " + Arrays.toString(myNumbers[i]));
+        }
+   
+    }
+}
