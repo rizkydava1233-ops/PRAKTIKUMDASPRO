162c31c172913859df014232b8d0445fdfc61c74
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-12-02 10:49:20 +0700

    tugas

---
+++ jpbsheet12/TUGAS/CubeStudentIDNumber.java
@@ -0,0 +1,40 @@
+package jpbsheet12.TUGAS;
+import java.util.Scanner;
+
+public class CubeStudentIDNumber {
+    
+    // Function to calculate area of rectangle
+    static int calculateArea(int length, int width) {
+        int area = length * width;
+        return area;
+    }
+    
+    // Function to calculate volume of cube/box
+    static int calculateVolume(int length, int width, int height) {
+        int volume = calculateArea(length, width) * height;
+        return volume;
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("=== Cube Area and Volume Calculator ===");
+        
+        System.out.print("Input length = ");
+        int length = input.nextInt();
+        
+        System.out.print("Input width = ");
+        int width = input.nextInt();
+        
+        System.out.print("Input height = ");
+        int height = input.nextInt();
+        
+        int area = calculateArea(length, width);
+        System.out.println("\nArea of the rectangle = " + area);
+        
+        int volume = calculateVolume(length, width, height);
+        System.out.println("Volume of the cube = " + volume);
+        
+        input.close();
+    }
+}
