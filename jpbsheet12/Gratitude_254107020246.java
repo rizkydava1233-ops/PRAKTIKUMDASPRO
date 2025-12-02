d20dcf75ab5ea28c40c82fd7629c243e49405abb
rizkydava1233-ops <rizkydava1233@gmail.com>
2025-12-02 08:41:15 +0700

    tugas1

---
+++ jpbsheet12/Gratitude_254107020246.java
@@ -0,0 +1,11 @@
+package jpbsheet12;
+
+public class Gratitude_254107020246 {
+    public static void sayThankyou() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+                         "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }    
+}
